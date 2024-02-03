afccdb99d6ff9df5c5e51bf1b682544f94fcc569
lingemily <152042718+lingemily@users.noreply.github.com>
2024-02-03 06:58:07 +0000

    modify

--- GUI/mail.py
+++ GUI/mail.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import pandas as pd
 import requests
-from stream-autorefresh import st_autorefresh
+from streamlit_autorefresh import st_autorefresh
+
 
 st_autorefresh(interval=5000)
 
